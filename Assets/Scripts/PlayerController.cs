573f1226b268b715d2bfdd6204ef1d5b90b97eb7
imadake398yen <imadake398yen@gmail.com>
2015-02-14 05:19:09 +0900

    Finish create text vol.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public GameObject fpsCamera;
+	// 使用する変数
+	public GameObject playerCamera;
 	public GameObject sparks;
 	public GameObject hitSound;
 	public AudioSource audio;
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour {
 
 
 	void Awake () {
+		// マウスカーソルをなくす
 		Screen.lockCursor = true;
   		Screen.showCursor = false;
 	}
@@ -20,20 +22,18 @@ public class PlayerController : MonoBehaviour {
 	
 	void Update () {
 
-		Debug.Log(Input.mousePosition);
-
+		// 左クリックした瞬間
 		if (Input.GetMouseButtonDown(0)) {
 			sparks.SetActive(true);
 		}
-
+		// 左クリックを話した瞬間
 		if (Input.GetMouseButtonUp(0)) {
 			sparks.SetActive(false);
 		}
-
-		if (Input.GetMouseButton(0)){ 
+		// 左クリック押し続けてる間
+		if (Input.GetMouseButton(0)) { 
 			shotInterval -= Time.deltaTime;
 			if (shotInterval < 0) {
-				audio.PlayOneShot(audio.clip);
 				shotInterval = 0.1f;
 				Shot();
 			}
@@ -43,10 +43,11 @@ public class PlayerController : MonoBehaviour {
 
 
 	void Shot () {
+		audio.PlayOneShot(audio.clip);
 		Vector3 center = new Vector3(Screen.width/2, Screen.height/2, 0);
-		ray = fpsCamera.camera.ScreenPointToRay(center);
+		ray = playerCamera.camera.ScreenPointToRay(center);
 		if (Physics.Raycast(ray,out hit,100)) {
-			Debug.Log(hit.point);
+			// Debug.Log(hit.point);
 			GameObject se = Instantiate (hitSound, hit.point, Quaternion.identity) as GameObject;
 			Destroy(se.gameObject, 0.6f);
 		}
