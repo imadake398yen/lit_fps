9a576a7de7c011fbd8580527bf5439bc5d09d143
imadake398yen <imadake398yen@gmail.com>
2015-02-17 00:20:52 +0900

    Add enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,6 +5,31 @@ public class EnemyController : MonoBehaviour {
 	
 	public int hitPoint = 10;
 	public GameObject effect;
+	public Transform[] points;
+	int currentPoint = 0;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () {
+		
+		Vector3 pos = points[currentPoint].position;
+
+		// 目的地に達したら次の目的地をセットする
+		if (Vector3.Distance(transform.position, pos) < 0.5f) {
+			if (currentPoint < points.Length - 1) {
+				currentPoint++;
+			} else {
+				currentPoint = 0;
+			}
+		}
+		
+		// セットした目的地に障害物を避けながら向かうコード
+		agent.SetDestination(points[currentPoint].position);
+	
+	}
 			
 	void ReceiveDamage (int damage) {
 		print("hit");
