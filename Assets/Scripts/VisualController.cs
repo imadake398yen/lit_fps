687b05d9da5c7f6e6c93dcce6fa8a85603f11f59
imadake398yen <imadake398yen@gmail.com>
2015-02-22 23:09:10 +0900

    delete takusann kesita

---
+++ Assets/Scripts/VisualController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualController : MonoBehaviour {
+
+	public Vector3 colorselect;
+
+	void Start () {
+		MeshFilter mf = GetComponent<MeshFilter>();
+	    mf.mesh.SetIndices(mf.mesh.GetIndices(0),MeshTopology.LineStrip,0);
+	}
+
+	void Update () {
+		colorselect.x=Random.Range(1f,6f);
+		colorselect.y=Random.Range(1f,6f);
+		colorselect.z=Random.Range(1f,6f);
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+  		Color selected = new Color(colorselect.x,colorselect.y,colorselect.z);
+  		mr.material.color = selected;
+	}
+}
---
+++ Assets/zDamy/Unko.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unko : MonoBehaviour {
+	
+	public int hitPoint = 10;
+	public GameObject effect;
+	public Transform[] points;
+	int currentPoint = 0;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () {
+		
+		Vector3 pos = points[currentPoint].position;
+
+		// 目的地に達したら次の目的地をセットする
+		if (Vector3.Distance(transform.position, pos) < 0.5f) {
+			if (currentPoint < points.Length - 1) {
+				currentPoint++;
+			} else {
+				currentPoint = 0;
+			}
+		}
+		
+		// セットした目的地に障害物を避けながら向かうコード
+		agent.SetDestination(points[currentPoint].position);
+	
+	}
+			
+	void ReceiveDamage (int damage) {
+		print("hit");
+		hitPoint -= damage;
+		if (hitPoint <= 0) {
+			Instantiate (effect, transform.position, Quaternion.identity);
+			Destroy (gameObject);
+		}
+	}	
+
+}
